7f397b4210b12e300a3c8dda8e02f9c86f6450b8
hax0r <39062772+widhisec@users.noreply.github.com>
2019-05-06 20:05:18 +0700

    Update checker.sh

--- checker.sh
+++ checker.sh
@@ -6,7 +6,7 @@ RED=$(tput setaf 1)
 NORMAL=$(tput sgr0)
 CYAN=$(tput setaf 6)
 main(){
-	     get=$(curl -s "https://verify-email.org/" -L | grep --text '<meta name="csrf-token" content=".*">' | grep -Po 'content="\K.*?(?=".*)')
+	 get=$(curl -s "https://verify-email.org/" -L | grep --text '<meta name="csrf-token" content=".*">' | grep -Po 'content="\K.*?(?=".*)')
          get1=$(curl --silent "https://verify-email.org/home/verify-as-guest/$empas" --compressed --socks5 ${soc} \
          -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64; rv:66.0) Gecko/20100101 Firefox/66.0" \
          -H "Accept: application/json, text/plain, */*" \
@@ -25,7 +25,7 @@ head(){
 printf "${CYAN}[+]\e[1;92msocks5 mana? = \e[0m"; read soc
 printf "${CYAN}[+]\e[1;92mlistny mana? = \e[0m"; read epas
 if [[ ! -e $epas  ]]; then
-    echo "file not found [!]"
+    echo "file empas not found [!]"
     exit 1
 fi
 (
