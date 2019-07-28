1b66c55ce0ac17f39af10c50d3ce50123a329164
hax0r <39062772+widhisec@users.noreply.github.com>
2019-07-28 12:55:41 +0700

    Add files via upload

--- email-checker.sh
+++ email-checker.sh
@@ -12,7 +12,7 @@ fi
 if [[ -z $(command -v grep) ]]; then
    echo -e "need package grep ✓"
 fi
-main(){
+main2(){
 	 get=$(curl --silent "https://verify-email.org/" -L | grep --text '<meta name="csrf-token" content=".*">' | grep -Po 'content="\K.*?(?=".*)')
          get1=$(curl --silent "https://verify-email.org/home/verify-as-guest/$empas" --compressed --socks5 ${soc} \
          -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64; rv:66.0) Gecko/20100101 Firefox/66.0" \
@@ -22,7 +22,7 @@ main(){
          -H "X-XSRF-TOKEN: ${get}" \
          -H "Connection: keep-alive" | grep -Po '(?<="response":)(.*?)(?=})' | grep -Po '"log"\s*:\s*"\K([^"]*)')
         printf "[+] $get1 -> $empas \n"
-        printf "[VALID] | $empas \n" >> VALID.txt
+        printf "$get1 | $empas \n" >> ini.txt
         #if [[ $get1 =~ "Success" ]]; then
             #printf "[${GREEN}VALID] ${NORMAL}$empas \n"
             #printf "[VALID] | $empas \n" >> VALID.txt
@@ -30,29 +30,22 @@ main(){
        	    #printf "[${RED}ERORR] ${NORMAL}$empas"
        #fi
 }
-head(){
+sl33p=5
+con=1
+threads=7
 printf "${CYAN}[+]\e[1;92msocks5 mana? = \e[0m"; read soc
 printf "${CYAN}[+]\e[1;92mlistny mana? = \e[0m"; read epas
-printf "${CYAN}[+]\e[1;92mtotal list = `wc -l $epas`\e[0m \n"
+#printf "${CYAN}[+]\e[1;92mtotal list" = $u(wc -l $epas)
 if [[ ! -e $epas  ]]; then
     echo "file empas not found [!]"
     exit 1
 fi
-(
-for empas in $(cat $epas); do
-    startline=1 ###
-    endline="30"
-    counter=0
-    default_threads="10"
-    threads="${30:-${default_threads}}"
-    token=$(($counter+1))
-    let token++
-    let counter++
-    let startline+=$threads
-    let endline+=$threads
-main "$empas" &
+for empas in $(cat $epas);do
+    fast=$(expr $con % $threads)
+    if [[ $fast == 0 && $con > 0 ]]; then
+       sleep $sl33p
+       printf "[loading] \n"
+     fi
+     main2 "$soc" "$epas"
 done
 wait
-)
-}
-head
